5baf402e87ce1ad1da4de6729f27502c8a9d1466
Agustin Grigaliunas <agrigaliunas@ibm.com>
2024-04-26 20:21:05 -0300

    Agregado cantidad a carrito

--- api2024grupo11/src/components/Cards/CartItemCard.jsx
+++ api2024grupo11/src/components/Cards/CartItemCard.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DecrementItemButton } from '../Buttons/DecrementItemButton'
+import { IncrementItemButton } from '../Buttons/IncrementItemButton'
 
 export const CartItemCard = (producto) => {
+
+  const [cantidadItem, setCantidadItem] = useState(producto.cantidad)
+
+  const handleIncrementItem = () => {
+    setCantidadItem(cantidadItem+1)
+  }
+
+  const handleDecrementItem = () => {
+    if (cantidadItem === 0 ) {
+      return;
+    }
+    setCantidadItem(cantidadItem-1)
+  }
+
   return (
-    <div className='flex flex-row bg-slate-200 w-[50vh] h-[8vh] items-center shadow rounded-md px-2 gap-4'>
-        <span className='font-semibold'>{producto.text}</span>
-        <span>{producto.cantidad}</span>
+    <div className='overflow-hidden flex flex-row bg-slate-200 w-[50vh] h-[8vh] items-center justify-between shadow rounded-md px-5 gap-4'>
+        {/* cambiar width del text si se hace overflow o no entra :) */}
+        <span className='font-light text-center'>{producto.text}</span>
+        <div className='grid grid-cols-2 items-center gap-2 border-l border-slate-300 pl-2'>
+          <span className='font-light text-sm'>Cantidad</span>
+          <div className='flex flex-row items-center gap-1'>
+            <DecrementItemButton onDecrement={handleDecrementItem}/>
+            <span className='text-sm font-normal'>{cantidadItem}</span>
+            <IncrementItemButton onIncrement={handleIncrementItem}/>
+          </div>
+
+        </div>
     </div>
   )
 }
--- api2024grupo11/src/screens/CarritoScreen.jsx
+++ api2024grupo11/src/screens/CarritoScreen.jsx
@@ -6,9 +6,9 @@ export const CarritoScreen = () => {
   return (
     <div className='text-black p-5'>
       <PageTitle text="Carrito de Compras"/>
-      <div className='flex flex-col w-full h-full gap-3 justify-center'>
+      <div className='flex flex-col w-full h-full gap-3 justify-center px-16'>
         <CartItemCard text="Silla Gamer" cantidad={1}/>
-        <CartItemCard text="Silla Ejecutiva" cantidad={2}/>
+        <CartItemCard text="Silla Ejecutiva" cantidad={1}/>
       </div>
     </div>
   )
