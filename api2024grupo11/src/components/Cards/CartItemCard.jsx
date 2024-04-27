923c6ae1af14616fc2bc8bf4c9c1743b2760339b
Agustin Grigaliunas <agrigaliunas@ibm.com>
2024-04-26 21:00:41 -0300

    Agregado suma del total por item

--- api2024grupo11/src/components/Cards/CartItemCard.jsx
+++ api2024grupo11/src/components/Cards/CartItemCard.jsx
@@ -5,30 +5,44 @@ import { IncrementItemButton } from '../Buttons/IncrementItemButton'
 export const CartItemCard = (producto) => {
 
   const [cantidadItem, setCantidadItem] = useState(producto.cantidad)
+  const [totalItem, setTotalItem] = useState(cantidadItem * producto.precio)
 
   const handleIncrementItem = () => {
-    setCantidadItem(cantidadItem+1)
+    const newCantidadItem = cantidadItem + 1;
+    setCantidadItem(newCantidadItem);
+    setTotalItem(newCantidadItem * producto.precio);
   }
 
   const handleDecrementItem = () => {
-    if (cantidadItem === 0 ) {
+    if (cantidadItem === 0) {
       return;
     }
-    setCantidadItem(cantidadItem-1)
+    const newCantidadItem = cantidadItem - 1;
+    setCantidadItem(newCantidadItem);
+    setTotalItem(newCantidadItem * producto.precio);
   }
 
   return (
-    <div className='overflow-hidden flex flex-row bg-slate-200 w-[50vh] h-[8vh] items-center justify-between shadow rounded-md px-5 gap-4'>
-        {/* cambiar width del text si se hace overflow o no entra :) */}
-        <span className='font-light text-center'>{producto.text}</span>
-        <div className='grid grid-cols-2 items-center gap-2 border-l border-slate-300 pl-2'>
-          <span className='font-light text-sm'>Cantidad</span>
-          <div className='flex flex-row items-center gap-1'>
-            <DecrementItemButton onDecrement={handleDecrementItem}/>
-            <span className='text-sm font-normal'>{cantidadItem}</span>
-            <IncrementItemButton onIncrement={handleIncrementItem}/>
+    <div className='overflow-hidden bg-slate-200 h-[8vh] shadow rounded-md px-3 flex items-center'>
+        <div className='grid grid-cols-3 items-center w-full text-xs'>
+          {/* col 1 */}
+          <span className='font-light text-center mx-2'>{producto.text}</span>
+          {/* col 2 */}
+          <div className='flex flex-row items-center border-l border-r justify-center border-slate-300 gap-2'>
+            <div className='flex flex-row items-center'>
+              <span className='font-light px-2'>Cantidad</span>
+              <div className='flex flex-row items-center gap-1'>
+                <DecrementItemButton onDecrement={handleDecrementItem}/>
+                <span className='text-sm font-light'>{cantidadItem}</span>
+                <IncrementItemButton onIncrement={handleIncrementItem}/>
+              </div>
+            </div>
+            <span className='font-semibold'>${totalItem}</span>
           </div>
-
+          {/* col 3 */}
+          <div className='w-full text-right'>
+            <button className='text-slate-500 text-[10px] hover:text-black'>Eliminar del carrito</button>
+          </div>
         </div>
     </div>
   )
--- api2024grupo11/src/screens/CarritoScreen.jsx
+++ api2024grupo11/src/screens/CarritoScreen.jsx
@@ -3,12 +3,21 @@ import { PageTitle } from '../components/Titles/PageTitle'
 import { CartItemCard } from '../components/Cards/CartItemCard'
 
 export const CarritoScreen = () => {
+
+  const total = 45;
+
   return (
     <div className='text-black p-5'>
       <PageTitle text="Carrito de Compras"/>
-      <div className='flex flex-col w-full h-full gap-3 justify-center px-16'>
-        <CartItemCard text="Silla Gamer" cantidad={1}/>
-        <CartItemCard text="Silla Ejecutiva" cantidad={1}/>
+      <div className='grid grid-cols-2 gap-2 h-full'>
+        <div className='flex flex-col w-full h-full gap-3 justify-center py-5'>
+          <CartItemCard text="Silla Gamer 410mm x 219mm bla bla bllalla" cantidad={1} precio={2}/>
+          <CartItemCard text="Silla Ejecutiva" cantidad={1} precio={5}/>
+        </div>
+        <div className='py-5 text-white flex flex-col px-2 border-l border-slate-300 h-full'>
+          <span className='font-bold'>Información del carrito</span>
+          <span className='font-normal'>Total estimado: <span className='text-black'>${total}</span></span>
+        </div>
       </div>
     </div>
   )
