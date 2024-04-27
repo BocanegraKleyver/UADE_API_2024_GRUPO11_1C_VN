be9591a9ebf724f07427466f3fdd8efaab233918
Agustin Grigaliunas <agrigaliunas@ibm.com>
2024-04-26 21:32:24 -0300

    Agregado boton de comprar

--- api2024grupo11/src/components/Cards/CartItemCard.jsx
+++ api2024grupo11/src/components/Cards/CartItemCard.jsx
@@ -1,49 +1,48 @@
-import React, { useState } from 'react'
-import { DecrementItemButton } from '../Buttons/DecrementItemButton'
-import { IncrementItemButton } from '../Buttons/IncrementItemButton'
+import React from 'react';
+import { DecrementItemButton } from '../Buttons/DecrementItemButton';
+import { IncrementItemButton } from '../Buttons/IncrementItemButton';
 
-export const CartItemCard = (producto) => {
-
-  const [cantidadItem, setCantidadItem] = useState(producto.cantidad)
-  const [totalItem, setTotalItem] = useState(cantidadItem * producto.precio)
+export const CartItemCard = ({ producto, onCantidadChange }) => {
 
   const handleIncrementItem = () => {
-    const newCantidadItem = cantidadItem + 1;
-    setCantidadItem(newCantidadItem);
-    setTotalItem(newCantidadItem * producto.precio);
-  }
+    const newCantidadItem = producto.cantidad + 1;
+    onCantidadChange(newCantidadItem);
+  };
 
   const handleDecrementItem = () => {
-    if (cantidadItem === 0) {
+    if (producto.cantidad === 0) {
       return;
     }
-    const newCantidadItem = cantidadItem - 1;
-    setCantidadItem(newCantidadItem);
-    setTotalItem(newCantidadItem * producto.precio);
-  }
+    const newCantidadItem = producto.cantidad - 1;
+    onCantidadChange(newCantidadItem);
+  };
+
+  const eliminarItemDelCarrito = () => {
+    alert("Feature a desarrollar :(");
+  };
 
   return (
     <div className='overflow-hidden bg-slate-200 h-[8vh] shadow rounded-md px-3 flex items-center'>
         <div className='grid grid-cols-3 items-center w-full text-xs'>
           {/* col 1 */}
-          <span className='font-light text-center mx-2'>{producto.text}</span>
+          <span className='font-light text-center mx-2 overflow-hidden'>{producto.text}</span>
           {/* col 2 */}
           <div className='flex flex-row items-center border-l border-r justify-center border-slate-300 gap-2'>
             <div className='flex flex-row items-center'>
               <span className='font-light px-2'>Cantidad</span>
               <div className='flex flex-row items-center gap-1'>
                 <DecrementItemButton onDecrement={handleDecrementItem}/>
-                <span className='text-sm font-light'>{cantidadItem}</span>
+                <span className='text-sm font-light'>{producto.cantidad}</span>
                 <IncrementItemButton onIncrement={handleIncrementItem}/>
               </div>
             </div>
-            <span className='font-semibold'>${totalItem}</span>
+            <span className='font-semibold'>${producto.cantidad * producto.precio}</span>
           </div>
           {/* col 3 */}
           <div className='w-full text-right'>
-            <button className='text-slate-500 text-[10px] hover:text-black'>Eliminar del carrito</button>
+            <button onClick={eliminarItemDelCarrito} className='text-slate-500 text-[10px] hover:text-black'>Eliminar del carrito</button>
           </div>
         </div>
     </div>
-  )
-}
+  );
+};
