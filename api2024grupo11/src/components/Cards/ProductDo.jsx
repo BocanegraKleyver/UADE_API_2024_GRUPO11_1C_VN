f4cdb6537e1095a6961641e8726869fbb31a6e45
fedemartin25 <107190447+fedemartin25@users.noreply.github.com>
2024-05-02 15:12:23 -0300

    css

--- api2024grupo11/src/components/Cards/ProductDo.jsx
+++ api2024grupo11/src/components/Cards/ProductDo.jsx
@@ -2,11 +2,15 @@ import './ProductDo.css';
 
 function ProductDo({value}) {
     return(
-        <div >
+        <div class="producto">
             <img className="producto-imagen" src={value.imagen} alt={value.titulo}></img>
             <div className="producto-detalles">
                 <h3 className="producto-titulo">{value.titulo}</h3>
+                <header class="paralelo">
                 <p className="producto-precio">$ {value.precio}</p>
+                <p className="producto-cantidad">Stock: {value.cantidad}</p>
+                </header>
+                <p className="producto-descripcion">{value.descripcion}</p>
                 <button className="producto-agregar" id={value.id}>Agregar</button>
             </div>
         </div>
