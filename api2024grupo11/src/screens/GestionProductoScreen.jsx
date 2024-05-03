36b5f469cb0a2cea6fe4cfdd071dc9770cc3f343
BocanegraKleyver <kbocanegracisneros@uade.edu.ar>
2024-05-03 11:36:47 -0300

    03-05-2024 table stock

--- api2024grupo11/src/screens/GestionProductoScreen.jsx
+++ api2024grupo11/src/screens/GestionProductoScreen.jsx
@@ -15,8 +15,8 @@ export const GestionProductoScreen = () => {
     
     const [id, setId] = useState();
     const [titulo, setTitulo] = useState();
-    const [categoria, setCategoria] = useState([]);
-    const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('');
+    const [categoria, setCategoria] = useState('');
+    // const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('');
     const [imagen_1, setImagen_1] = useState('');
     const [imagen_2, setImagen_2] = useState('');
     const [descripcion, setDescripcion] = useState();
@@ -24,7 +24,7 @@ export const GestionProductoScreen = () => {
     const [cantidad, setCantidad] = useState();   
     const [producto, setProductos] = useState([]);
     const navegate = useNavigate();
-    
+    const itemsCategoria = ["jardin","comedor","Gamer"];
 
     /////////////// Bloque de  los llamados al JSON (features base de datos) ///////////////////
     
@@ -41,17 +41,22 @@ export const GestionProductoScreen = () => {
     //Handles setID
     const handleSetID = event => {
         setId(event.target.value);
-    }
+    };
 
     //Handles setTitulo
     const handleSetTitulo = event => {
         setTitulo(event.target.value);
-    }
+    };
 
-    //Handles Selecciona categoria
-    const handleCategoriaSeleccionadaChange = event => {
-        setCategoriaSeleccionada(event.target.value);
-    }
+    // //Handles Selecciona categoria para cuando usemos backend
+    // const handleCategoriaSeleccionadaChange = event => {
+    //     setCategoriaSeleccionada(event.target.value);
+    // };
+
+    const handleSetCategoria = event => {
+        setCategoria(event.target.value);
+    };
+
 
     // Handles para capturar la img del producto 1 que subiremos al json.
     const handleFileFotoChange_1 = (e) => {
@@ -82,17 +87,17 @@ export const GestionProductoScreen = () => {
     //Handles setDescripcion
     const handleSetDescripcion = event => {
         setDescripcion(event.target.value);
-    }
-
+    };
+    
     //Handles setPrecio
     const handleSetPrecio = event => {
         setPrecio(event.target.value);
-    }       
+    };      
         
     //Handles setCantidad
     const handleSetCantidad = event => {
         setCantidad(event.target.value);
-    }
+    };
 
     //Handles ALTA productos
     const handleAltaProducto = () => {
@@ -159,9 +164,9 @@ return (
 
 
     <div class="nice-header header-fix-top small"> <div class="logo">
-        <h1>Gestion de Productos</h1>
+        <h1>Gestión de Productos</h1>
         <hr></hr>
-        <h5>En este espacio vas a poder dar de alta tus productos. Ademas podras realizar modificaciones en cuanto al stock que quieras vender o eliminarlos.</h5>
+        <h5>En este espacio vas a poder dar de alta tus productos. Además, podrás realizar modificaciones en cuanto al stock que quieras vender o eliminarlos.</h5>
         </div>
     </div>
     <hr></hr>
@@ -180,30 +185,15 @@ return (
                 </div>
 
                 <div className="form_Gestion_Producto_group">
-
-                    <input className="form_Gestion_Producto_input" type="text" id="categoria_Producto" value={categoriaSeleccionada} placeholder="SELECCIONE CATEGORIA" readOnly></input>
-                    <label className="form_Gestion_Producto_label"> : CATEGORIA </label>
-                    <select className="form_Gestion_Producto_input" id="Selector_categoria_dropdown" value={categoriaSeleccionada} onChange={handleCategoriaSeleccionadaChange}>
-                        <option value=""> Seleccione </option>
-                        {categoria.map((categoria, index) => (
-                            <option key={index} value={categoria.descripcion}>{categoria.descripcion} </option>
-                        ))}
-                    </select>
-                </div>
-                <br/>
-
-                <div className="form_Gestion_Producto_group">
-                    <label className="form_Gestion_Producto_label"> IMAGEN 1:  </label>
-                    <input className="form_Gestion_Producto_input" type="file" id="imagen_Producto_1" onChange={handleFileFotoChange_1} accept="image/jpeg,image/png" data-testid="fileInput"  ></input>                   
+                    <label className="form_Gestion_Producto_label"> FOTO 1 DEL PRODUCTO:  </label>
+                    <input className="form_Gestion_Producto_input" type="file" id="imagen_Producto_1" onChange={handleFileFotoChange_1} accept="image/jpeg,image/png" data-testid="fileInput" ></input>                   
                 </div>
 
                 <div className="form_Gestion_Producto_group">
-                    <label className="form_Gestion_Producto_label"> IMAGEN 2:  </label>
-                    <input className="form_Gestion_Producto_input" type="file" id="imagen_Producto_2" onChange={handleFileFotoChange_2} accept="image/jpeg,image/png" data-testid="fileInput"  ></input>                   
+                    <label className="form_Gestion_Producto_label"> FOTO 2 DEL PRODUCTO:  </label>
+                    <input className="form_Gestion_Producto_input" type="file" id="imagen_Producto_2" onChange={handleFileFotoChange_2} accept="image/jpeg,image/png" data-testid="fileInput" ></input>                   
                 </div>
-
-                <br/>
-            
+    
                 <div className="form_Gestion_Producto_group">
                         <input className="form_Gestion_Producto_input" type="text" id="descripcion_Producto" value={descripcion} placeholder="DESCRIPCION" onChange={handleSetDescripcion}></input>
                         <label className="form_Gestion_Producto_label"> : DESCRIPCION</label>
@@ -218,8 +208,31 @@ return (
                         <input className="form_Gestion_Producto_input" type="number" id="cantidad_Producto" value={cantidad} placeholder="CANTIDAD" onChange={handleSetCantidad}></input>
                         <label className="form_Gestion_Producto_label"> : CANTIDAD</label>
                 </div>
-                <br/>
                 
+                <div className="form_Gestion_Producto_group">
+                        <select className="form_Gestion_Producto_input" id="Selector_categoria_dropdown" value={categoria} onChange={handleSetCategoria}>
+                            <option value=""> Seleccione </option>
+                            {itemsCategoria.map((items, index) => (
+                                <option key={index}  value={items}>{items} </option>
+                            ))}
+                        </select>
+                        <label className="form_Gestion_Producto_label"> : CATEGORIA </label>
+                </div>
+                <br></br>
+
+                {/* Categoria para usar con valores del JSON    
+                <div className="form_Gestion_Producto_group">
+                    <input className="form_Gestion_Producto_input" type="text" id="categoria_Producto" value={categoriaSeleccionada} placeholder="SELECCIONE CATEGORIA" readOnly></input>
+                    <label className="form_Gestion_Producto_label"> : CATEGORIA </label>
+                    <select className="form_Gestion_Producto_input" id="Selector_categoria_dropdown" value={categoriaSeleccionada} onChange={handleCategoriaSeleccionadaChange}>
+                        <option value=""> Seleccione </option>
+                        {categoria.map((categoria, index) => (
+                            <option key={index} value={categoria.descripcion}>{categoria.descripcion} </option>
+                        ))}
+                    </select>
+                </div>
+                <br/> */}
+
             <AltaProductoButton handleClickAltaProducto={handleAltaProducto}></AltaProductoButton>
             <hr></hr>
 
@@ -230,12 +243,12 @@ return (
                         <th>ID</th>
                         <th>Producto</th>
                         <th>Precio</th>
-                        {/* <th>Categoria</th> */}
+                        <th>Categoria</th>
                         <th>Imagen 1</th>
                         <th>Imagen 2</th>
-                        <th>Descripcion</th>
+                        <th>Descripción</th>
                         <th>Cantidad</th>
-                        <th>Opcion</th>
+                        <th>Opción</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -244,7 +257,7 @@ return (
                         <td>{producto.id}</td>
                         <td>{producto.titulo}</td>
                         <td>${producto.precio}</td>
-                        {/* <td>{producto.categoria}</td>  */}
+                        <td>{producto.categoria}</td> 
                         <td>{producto.imagen_1 && (<img src={producto.imagen_1} alt="Uploaded" style={{ maxWidth: '75px' }} />)}</td>
                         <td>{producto.imagen_2 && (<img src={producto.imagen_2} alt="Uploaded" style={{ maxWidth: '75px' }} />)}</td>
                         <td>{producto.descripcion}</td>
@@ -258,8 +271,8 @@ return (
                         </td>
                     </tr>
                 ))}
-                </tbody> 
-            </table>
+                </tbody>
+            </table> 
             <Outlet></Outlet>
             </form>
         </div>
