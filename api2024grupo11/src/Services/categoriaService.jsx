4b498fb18c66598d2de67e249893607a656aaa16
Agustin Grigaliunas <agrigaliunas@ibm.com>
2024-05-03 21:21:46 -0300

    Added async await

--- api2024grupo11/src/Services/categoriaService.jsx
+++ api2024grupo11/src/Services/categoriaService.jsx
@@ -1,42 +1,50 @@
-export const getCategoria = () => {
-    return(
-        fetch("http://localhost:3000/categoria")
-        .then((Response) => Response.json())
-        .catch(error => console.log('error',error))
-        .finally(() => console.log('promise is finished'))
-    )
+export const getCategoria = async () => {
+    try {
+        const response = await fetch("http://localhost:3000/categoria");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    } finally {
+        console.log('promise is finished');
+    }
 };
 
-export const altaCategoria = (id,descripcion) => {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+export const altaCategoria = async (id, descripcion) => {
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-    var raw = JSON.stringify({
-        "id": id,
-        "descripcion": descripcion
-    });
-    
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
+        const raw = JSON.stringify({
+            "id": id,
+            "descripcion": descripcion
+        });
+        
+        const requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
 
-    fetch("http://localhost:3000/categoria", requestOptions)
-    .then(Response => Response.text())
-    .then(result => console.log(result))
-    .cath(error => console.log('error', error));
+        const response = await fetch("http://localhost:3000/categoria", requestOptions);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.log('error', error);
+    }
 };
 
-export const eliminarCategoria = (id) => {
-    
-    var requestOptions = {
-        method: 'DELETE',
-    };
+export const eliminarCategoria = async (id) => {
+    try {
+        const requestOptions = {
+            method: 'DELETE',
+        };
 
-    fetch("http://localhost:3000/categoria/" + id, requestOptions)
-    .then(Response => Response.text())
-    .cath(error => console.log('error', error));
-
+        const response = await fetch("http://localhost:3000/categoria/" + id, requestOptions);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.log('error', error);
+    }
 };
--- api2024grupo11/src/Services/productosService.jsx
+++ api2024grupo11/src/Services/productosService.jsx
@@ -1,108 +1,111 @@
-export const getProductos = () => {
-    return(
-        fetch("http://localhost:8000/productos")
-        .then((Response) => Response.json())
-        .catch(error => console.log('error',error))
-    )
+export const getProductos = async () => {
+    try {
+        const response = await fetch("http://localhost:8000/productos");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    }
 };
 
-export const altaProdcuto = (id,titulo,categoria,imagen_1,imagen_2,descripcion,precio,cantidad) => {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
- 
-    var raw = JSON.stringify({
-        "id": id,
-        "titulo": titulo,
-        "categoria": categoria,
-        "imagen_1":imagen_1,
-        "imagen_2":imagen_2,
-        "descripcion": descripcion,
-        "precio":parseFloat(precio),
-        "cantidad":parseInt(cantidad)
-    });
-   
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
- 
-    fetch("http://localhost:8000/productos", requestOptions)
-    .then(Response => Response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-};
- 
-export const eliminarProducto = (id) => {
-   
-    var requestOptions = {
-        method: 'DELETE',
-    };
- 
-    fetch("http://localhost:8000/productos/" + id, requestOptions)
-    .then(Response => Response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-};
- 
- 
-export const aumentarCantidad = (id, cantidad) => {
-    const nuevaCantidad = cantidad + 1;
-   
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
- 
-    const raw = JSON.stringify({
-        "cantidad": nuevaCantidad
-    });
-   
-    const requestOptions = {
-        method: 'PATCH',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
- 
-    fetch(`http://localhost:8000/productos/${id}`, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('La solicitud PATCH no fue exitosa.');
-            }
-            return response.json();
-        })
-        .then(result => console.log(result))
-        .catch(error => console.error('Error al aumentar la cantidad:', error));
+export const altaProdcuto = async (id, titulo, categoria, imagen_1, imagen_2, descripcion, precio, cantidad) => {
+    try {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "id": id,
+            "titulo": titulo,
+            "categoria": categoria,
+            "imagen_1": imagen_1,
+            "imagen_2": imagen_2,
+            "descripcion": descripcion,
+            "precio": parseFloat(precio),
+            "cantidad": parseInt(cantidad)
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        const response = await fetch("http://localhost:8000/productos", requestOptions);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.log('error', error);
+    }
 };
 
-export const decrementarCantidad = (id, cantidad) => {
-    const nuevaCantidad = cantidad - 1;
-   
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
- 
-    const raw = JSON.stringify({
-        "cantidad": parseInt(nuevaCantidad)
-    });
-    
-    const requestOptions = {
-        method: 'PATCH',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
- 
-    fetch(`http://localhost:8000/productos/${id}`, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('La solicitud PATCH no fue exitosa.');
-            }
-            return response.json();
-        })
-        .then(result => console.log(result))
-        .catch(error => console.error('Error al disminuir la cantidad:', error));
+export const eliminarProducto = async (id) => {
+    try {
+        var requestOptions = {
+            method: 'DELETE',
+        };
+
+        const response = await fetch("http://localhost:8000/productos/" + id, requestOptions);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.log('error', error);
+    }
 };
-export const decrementarCantidadEnN = async (producto, cantADecrementar) => {   
+
+export const aumentarCantidad = async (id, cantidad) => {
+    try {
+        const nuevaCantidad = cantidad + 1;
+
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "cantidad": nuevaCantidad
+        });
+
+        const requestOptions = {
+            method: 'PATCH',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        const response = await fetch(`http://localhost:8000/productos/${id}`, requestOptions);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error('Error al aumentar la cantidad:', error);
+    }
+};
+
+export const decrementarCantidad = async (id, cantidad) => {
+    try {
+        const nuevaCantidad = cantidad - 1;
+
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "cantidad": parseInt(nuevaCantidad)
+        });
+
+        const requestOptions = {
+            method: 'PATCH',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        const response = await fetch(`http://localhost:8000/productos/${id}`, requestOptions);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error('Error al disminuir la cantidad:', error);
+    }
+};
+
+export const decrementarCantidadEnN = async (producto, cantADecrementar) => {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -134,5 +137,3 @@ export const decrementarCantidadEnN = async (producto, cantADecrementar) => {
         console.error('Error al disminuir la cantidad:', error);
     }
 };
-
- 
