033162700aacfbb85eee3a6c3effc984720c21f7
fedemartin25 <107190447+fedemartin25@users.noreply.github.com>
2024-07-11 23:36:36 -0300

    cam

--- api2024grupo11/src/screens/ComprarScreen.jsx
+++ api2024grupo11/src/screens/ComprarScreen.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
+import { default as React, default as React, useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 import { addToCarrito, fetchCarritoByUserId } from '../Redux/CarritoSlice';
 import { fetchCategorias } from '../Redux/CategoriaSlice';
 import { fetchDescuentos } from '../Redux/DescuentoSlice';
 import { agregarItemAFavoritosLocalmente } from "../Redux/FavoritoSlice";
 import { fetchProductos, filterProductos } from '../Redux/ProductoSlice';
-
 
   export const ComprarScreen = () => {
     const dispatch = useDispatch();
